010cfe05c46d144d334402a435db36dd5bb1c3dc
Nazarena Camarota <nvcamarota.dev@gmail.com>
2025-02-11 20:26:26 -0300

    Consigna para desarrollar el catálogo de películas

---
+++ archivo.py
@@ -0,0 +1,34 @@
+"""
+FINAL INTEGRADOR - CATÁLOGO DE PELÍCULAS
+El objetivo consiste en desarrollar un programa que permita llevar un registro de películas, aplicando conceptos de programación orientada a objetos.
+
+El funcionamiento esperado es el siguiente:
+
+• Al ejecutar un programa se solicita ingresar el nombre del catálogo de películas.
+• Si el catálogo de películas no existe, se creará uno nuevo. Este catálogo se va a guardar en un archivo.txt donde, posteriormente, se guardarán las películas. Si el catálogo existe, se podrá seguir modificando el archivo.
+• Se debe mostrar un menú de opciones que permita realizar las siguientes operaciones:
+
+    1) Agregar película:
+    2) Listar películas
+    3) Eliminar catálogo de películas
+    4) Salir
+    
+Funcionamiento de las opciones:
+
+    • Agregar película: Se va a solicitar el nombre de la película y esta película se va a guardar en el archivo.txt
+    • Listar películas: Se va a mostrar todas las películas del catálogo y guardadas en el archivo.txt
+    • Eliminar catálogo: Elimina el archivo.txt que corresponde al catálogo de películas
+    • Salir: Debe finalizar el programa mostrando un mensaje al usuario
+    
+Implementación POO:
+El programa debe implementar programación orientada a objetos.
+Se solicita:
+
+    • Clase Película:
+        * Uno de sus atributos debe ser privado
+        
+    • Clase CatálogoPelícula:
+        * atributo nombre
+        * atributo ruta_archivo
+        * métodos: agregar, listar, eliminar
+"""
